74615b34d165afe6f49923ac2ce377b126af02b9
Emeric Mourot <emeric.mourot@terden.com>
2016-01-29 10:02:00 +0100

    Added VPS1 stuff

---
+++ VPS1-vps236945.ovh.net/150-docker-install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# <START OF CONFIG>
+config_app_name=docker-install
+config_default_install=yes
+# <END OF CONFIG>
+
+# required after <START OF CONFIG>/<END OF CONFIG> bloc
+source "var.cfg" 2>&1 /dev/null
+
+#
+# DOCKER
+#
+# last version installation
+
+# check if docker is already installed
+docker_version=`$SSHCMD "docker --version 2> /dev/null | head -n 1 | awk  '{print $3}'"`
+# remove trailing ,
+docker_version=${docker_version%,*}
+
+_d "Docker version <${docker_version}> detected"
+
+if [ -z "$docker_version" ] ; then
+    # install from wget (@see https://github.com/discourse/discourse/blob/master/docs/INSTALL-cloud.md)
+    _re "wget -qO- https://get.docker.com/ | sh" "docker installed" "docker installation failed"
+else
+    _e "Docker version ${docker_version} already installed"
+fi # docker not already installed
---
+++ VPS1-vps236945.ovh.net/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Run this script after editing your hostname config (xxx.cfg)
+
+
+
+/Users/emeric/Dev/LPE/VPS-Install/install.d/1-ssh-config.sh
+/Users/emeric/Dev/LPE/VPS-Install/install.d/10-hostinfo-get.sh
+/Users/emeric/Dev/LPE/VPS-Install/install.d/100-users-config.sh
+/Users/emeric/Dev/LPE/VPS-Install/install.d/12-dist-upgrade.sh
+/Users/emeric/Dev/LPE/VPS-Install/install.d/120-nginx-install.sh
+/Users/emeric/Dev/LPE/VPS-Install/install.d/14-ntpdate-install.sh
+/Users/emeric/Dev/LPE/VPS-Install/install.d/140-java-install.sh
+/Users/emeric/Dev/LPE/VPS-Install/install.d/150-docker-install.sh
+/Users/emeric/Dev/LPE/VPS-Install/install.d/180-monit-install.sh
